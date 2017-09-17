1753908368c32bbd22e57260a520b37b2a7230c6
jordynlm <jordynm@mit.edu>
2017-09-17 04:33:11 -0400

    Broken search stuff. google not defined

--- SearchFunctions.py
+++ SearchFunctions.py
@@ -5,3 +5,8 @@ Spyder Editor
 This is a temporary script file.
 """
 
+from googleplaces import GooglePlaces, types, lang
+
+YOUR_API_KEY = 'AIzaSyBNv3yuOPpRS5rB8An7ix8K2dTl-XM5UZY'
+
+google_places = GooglePlaces(YOUR_API_KEY)
