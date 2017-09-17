2438340bbf2c018b6cc1e5acda6746ae7e062f77
Tosin <tosino@mit.edu>
2017-09-17 08:58:13 -0400

    fixed indents

--- index.py
+++ index.py
@@ -16,16 +16,16 @@ app = Flask(__name__, static_url_path="")
 
 @app.route("/")
 def run_app():
-	return "Hello, world"
+    return "Hello, world"
 
 @app.route("/<path:path>")
 def status_file(path):
-	return app.send_static_file(path)
+    return app.send_static_file(path)
 
 #change start or dest
 @app.route("/api/seed", methods=['POST'])
 def set_loc():
-	coordinates = request.get_json(force=True)
+    coordinates = request.get_json(force=True)
     #start
     set_start(coordinates["start_coord"])
     set_dest(coordinates["end_coord"])
@@ -35,20 +35,20 @@ def set_loc():
 #change loc
 @app.route("/api/change_loc/", methods=['POST'])
 def change_loc():
-	params = request.get_json(force=True)
+    params = request.get_json(force=True)
 
     #start
-    if params[node] = "start":
-    	set_start(new_coord)
+    if params[node] == "start":
+        set_start(new_coord)
     else:
-    	set_dest(new_coord)
+        set_dest(new_coord)
     
     return
 
 #change car
 @app.route("/api/ride/", methods=['POST'])
 def change_car():
-	ride_type = request.get_json(force=True)
+    ride_type = request.get_json(force=True)
     return set_car(new_car)
 
 #query prices
