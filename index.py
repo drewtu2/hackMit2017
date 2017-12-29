22cd80aeb0458e0f97fd935e1f6d6b9e2e008357
Andrew Tu <drewtu2@yahoo.com>
2017-12-29 18:04:01 -0500

    fixed more bugs with security

--- index.py
+++ index.py
@@ -17,7 +17,7 @@ app = Flask(__name__, static_url_path="")
 
 @app.before_request
 def before_request():
-    if request.is_secure():
+    if request.is_secure:
         url = request.url.replace('http://', 'https://', 1)
         code = 301
     return redirect(url, code=code)
